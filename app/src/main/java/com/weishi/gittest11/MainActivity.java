b7173a3b974c81d443514571d9ca4a876f6df17d
zcy <401200196@qq.com>
2018-04-16 17:59:36 +0800

    featuer1

--- app/src/main/java/com/weishi/gittest11/MainActivity.java
+++ app/src/main/java/com/weishi/gittest11/MainActivity.java
@@ -2,6 +2,7 @@ package com.weishi.gittest11;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void feature1(View view) {
+        /**
+         * feature-1
+         * display HelloWorld
+         * @param view
+         */
     }
 }
